b0710e20cd181fd2d5cb82c379d2eabcfa05928f
Muhammad Ali Hasnain <mahasnai@uwaterloo.ca>
2023-09-17 23:15:21 -0400

    py2

---
+++ processing.py
@@ -0,0 +1,12 @@
+
+def tokenize_text(text):
+    # Tokenize the input text into words or sentences
+    pass
+
+def clean_text(text):
+    # Perform text cleaning and normalization
+    pass
+
+def preprocess_text(text):
+    # Perform preprocessing steps on the text
+    pass
